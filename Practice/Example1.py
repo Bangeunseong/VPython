3ea51a7f401b64a9f9245e10525a09fcad9efe16
Bangeunseong <bangeunseong@naver.com>
2023-03-07 14:57:58 +0900

    Commit

--- Practice/Example1.py
+++ Practice/Example1.py
@@ -2,4 +2,8 @@ from vpython import *
 
 scale_capacity = 5.0
 
-earth = sphere(pos = vec(0,0,0), radius = scale_capacity * 1241231, texture = textures.earth)
+r = 384400000
+G = 6.67e-11
+
+earth = sphere(pos = vec(0,0,0), radius = scale_capacity * 6371000, texture = textures.earth)
+moon = sphere(pos = vec(r,0,0), radius = scale_capacity * 1373000, color = color.white)
