b6e6e1c06b0cae45ecdf4780798283cd122b3f72
Bangeunseong <bangeunseong@naver.com>
2023-03-08 13:20:53 +0900

    Commit

--- Practice/Example1.py
+++ Practice/Example1.py
@@ -1,9 +1,11 @@
 from vpython import *
 
+
 scale_capacity = 5.0
 
 r = 384400000
 G = 6.67e-11
 
+#Example = cylinder(pos = vec(0,0,0), axis = vec(5,0,0), radius = 1, color = color.red)
 earth = sphere(pos = vec(0,0,0), radius = scale_capacity * 6371000, texture = textures.earth)
 moon = sphere(pos = vec(r,0,0), radius = scale_capacity * 1373000, color = color.white)
