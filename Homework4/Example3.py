806dc0b97d6e0253ac58a4dfebd5347c10f98ffc
Bangeunseong <bangeunseong@naver.com>
2023-03-28 15:47:22 +0900

    Commit

--- Homework4/Example3.py
+++ Homework4/Example3.py
@@ -1,2 +1,40 @@
 from vpython import *
 
+#**Number1**
+# What's the position of elevator after 10 sec passed?
+# -> 4m above the ground(offset)
+
+#**Number2**
+# Get sectors when elevator users felt less force than gravity!
+# -> 5~6sec, 7~8sec
+
+#**Number3**
+
+t = 0
+dt = 0.001
+
+elevator = box(pos = vec(0,0,0), size = vec(2,4,2), color = color.red)
+
+elevator.a = vec(0,2,0)
+elevator.v = vec(0,0,0)
+
+motion_graph = graph(title = 'position-time', xtitle = 't', ytitle = 'y')
+g_elevator = gcurve(graph = motion_graph, color = color.green)
+velocity_graph = graph(title = "velovity-time", xtitle = 't', ytitle = 'v')
+g_elevator_v = gcurve(graph = velocity_graph, color = color.cyan)
+
+while t <= 10:
+    rate(1/dt)
+    if t > 1 and t <= 2:
+        elevator.v += elevator.a*dt
+    elif t > 5 and t <= 6:
+        elevator.v -= elevator.a*dt
+    elif t > 7 and t <= 8:
+        elevator.v -= elevator.a*dt
+    elif t > 9 and t <= 10:
+        elevator.v += elevator.a*dt
+    elevator.pos += elevator.v*dt
+    g_elevator.plot(pos = (t,elevator.pos.y))
+    g_elevator_v.plot(pos = (t,elevator.v.y))
+    t += dt
+print(elevator.pos)
