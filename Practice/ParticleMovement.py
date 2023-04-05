3074dde59de67e142104dbe9d98c113aed0eed93
Bangeunseong <bangeunseong@naver.com>
2023-04-05 14:41:40 +0900

    ParticleMovement

--- Practice/ParticleMovement.py
+++ Practice/ParticleMovement.py
@@ -1,65 +1,48 @@
 from vpython import *
 
-#Same velocity movement
-ball = sphere(radius = 0.2)
-ball.pos = vec(-2,0,0)
-ball.v = vec(0.8,0,0)
+def biInterp(pos, dx, dy, grid):
+    cellx = int(pos.x/dx)
+    celly = int(pos.y/dy)
 
-t = 0
-dt = 0.01
-attach_arrow(ball, "v", shaftwidth = 0.1, color = color.green)
+    cellbox = box(pos = vec((cellx + 0.5)*dx, (celly+0.5)*dy, -0.1), length = dx, height = dy, width = 0.05)
+    
+    rx = pos.x - cellx*dx
+    ry = pos.y - celly*dy
+    resultx0 = grid[cellx][celly]*(dx - rx)/dx + grid[cellx + 1][celly]*rx/dx
+    resultx1 = grid[cellx][celly + 1]*(dx - rx)/dx + grid[cellx + 1][celly + 1]*rx/dx
+    result = resultx0*(dy-ry)/dy + resultx1*ry/dy
+    
+    return result
 
-while t<4:
-    rate(1/dt)
-    ball.pos += ball.v*dt
-    t += dt
+n = 20
+m = 20
+dx = 1
+dy = 1
 
+scene.center = vec(n*dx/2,m*dy/2,0)
 
-#Same acceleration movement
-ball = sphere(radius = 0.2)
-ball.pos = vec(-2,0,0)
+rList = []
+objList = []
+
+for i in range(0,n):
+    rList.append([])
+    objList.append([])
+    for j in range(0,m):
+        rList[i].append(vec(random(),random(),0))
+        objList[i].append(arrow(pos = vec(i*dx,j*dy,0),axis = rList[i][j], shaftwidth = 0.2))
+
+r_i = vec(random(), random(), 0)
+
+ball = sphere(pos = r_i, radius = 0.2, color = color.yellow, make_trail = True)
 ball.v = vec(0,0,0)
-ball.a = vec(0.35,0,0)
+attach_arrow(ball,"v")
+attach_trail(ball, color = color.red)
 
 t = 0
-dt = 0.01
-
-attach_arrow(ball,"v", shaftwidth = 0.1, color=color.green)
-attach_arrow(ball,"a", shaftwidth = 0.05, color=color.red)
-attach_trail(ball, type = 'points', pps = 5)
-
-while t<4:
-    rate(1/dt)
-    ball.v += ball.a*dt
+dt = 0.1
+while ball.pos.x < (n - 1)*dx and ball.pos.y < (m - 1)*dy:
+    rate(100)
+    ball.v = biInterp(ball.pos,dx,dy,rList)
     ball.pos += ball.v*dt
     t += dt
-
-
-#Curve movement
-ball = sphere(radius = 0.2)
-ground = box(pos = vec(0,-4,0), size = vec(15,-0.01,5))
-
-ball.pos = vec(-2,0,0)
-ball.v = vec(1,1,0)
-ball.a = vec(0,-0.35,0)
-
-t = 0
-dt = 0.01
-
-attach_arrow(ball, "v", shaftwidth = 0.1, color=color.green)
-attach_arrow(ball, "a", shaftwidth = 0.05, color=color.red)
-attach_trail(ball, type = 'points', pps = 5)
-#extra component Graph
-motion_graph = graph(title = 'position-time', xtitle = 't', ytitle = 'y')
-gbally = gcurve(graph = motion_graph)
-motion_graph2 = graph(title = "velocity-time", xtitle = "t", ytitle = "vy")
-gballvy = gcurve(color = color.green, graph = motion_graph2)
-
-while ball.pos.y > ground.pos.y:
-    rate(1/dt)
-    ball.v += ball.a*dt
-    ball.pos += ball.v*dt
-
-    gbally.plot(pos = (t,ball.pos.y))
-    gballvy.plot(pos = (t,ball.v.y))
-    t += dt
+print("loop out")
