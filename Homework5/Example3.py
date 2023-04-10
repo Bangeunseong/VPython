e79c96e7ea31e81982bf8419d3c23975f15a0958
Bangeunseong <bangeunseong@naver.com>
2023-04-10 13:58:39 +0900

    Homework5

---
+++ Homework5/Example3.py
@@ -0,0 +1,31 @@
+from vpython import *
+
+#Field
+s = sphere(pos = vec(0,0,0), radius = 1, color = color.red)
+arrow(axis = vec(4,0,0), color = color.white, shaftwidth = 0.1)
+arrow(axis = vec(0,4,0), color = color.white, shaftwidth = 0.1)
+#Time Field
+t = 0
+dt = 0.001
+
+#distance, velocity, acceleration vectors
+s.v = vec(2,0,0)
+s.a = s.v.cross(vec(0,0,-1))
+r = 0
+
+#position graph of sphere s
+motion_graph = graph(title = 'position-time', xtitle = 't', ytitle = 'y')
+g_bally = gcurve()
+
+
+while t < 10:
+    rate(1/dt)
+    s.pos += s.v*dt
+    s.v += s.a*dt
+    s.a = s.v.cross(vec(0,0,-1))
+    g_bally.plot(pos = (t,s.pos.y))
+    if r < s.pos.y:
+        r = s.pos.y
+    t += dt
+
+print(r/2)
