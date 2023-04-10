6cddda1d35f90d5516bd06f0dfb38b13d7ea3b2e
Bangeunseong <bangeunseong@naver.com>
2023-04-11 01:11:44 +0900

    Commit

--- Homework5/Example1.py
+++ Homework5/Example1.py
@@ -9,7 +9,7 @@ r = vec(R*cos(w*t),R*sin(w*t), 0)
 v = vec((-R)*w*sin(w*t), R*w*cos(w*t), 0)
 a = vec((-R)*(w**2)*cos(w*t), (-R)*(w**2)*sin(w*t), 0)
 
-r_arrow = arrow(axis = r, color = color.green, shaftwidth = 0.1)
+r_arrow = arrow(axis = r, color = color.green, shaftwidth = 0.02)
 v_arrow = arrow(axis = v, color = color.blue, shaftwidth = 0.02)
 a_arrow = arrow(axis = a, color = color.red, shaftwidth = 0.02)
 
