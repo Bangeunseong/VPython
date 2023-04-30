17887561bf165a808ace6820d0f3ee83f1d76f23
Bangeunseong <bangeunseong@naver.com>
2023-05-01 01:27:50 +0900

    Commit

--- Homework7/Example1.py
+++ Homework7/Example1.py
@@ -1,2 +1,5 @@
 from vpython import *
 
+serface = triangle(v0 = vertex(pos = vec(0,0,0), color = color.green), v1 = vertex(pos = vec(8,0,0), color = color.green), v2 = vertex(pos = vec(0,6,0), color = color.green))
+ball = sphere(pos = vec(0,6,0), mass = 1, radius = 0.1, color = color.white)
+
