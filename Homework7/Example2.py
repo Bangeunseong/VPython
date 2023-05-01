e5ef7eb44ce33997327e943cb0470c8f36647d88
Bangeunseong <bangeunseong@naver.com>
2023-05-01 12:35:22 +0900

    Update

--- Homework7/Example2.py
+++ Homework7/Example2.py
@@ -1,13 +1,12 @@
-from vpython import *
-
 #2
 #종단 속력 = 1/2*Fg/(Cd*P*A)
 #P = 1, A = pi*radius, Fg = mg
+from vpython import *
 
 scalefactor = 10
 
 raindrop = sphere(pos = vec(0,250,0), radius = 0.5*scalefactor, mass = 5*10**-7, v = vec(0,0,0), color = color.white)
-ground = box(pos = vec(0,-0.5,0), size = vec(8,1,8)*scalefactor, color = color.white)
+ground = box(pos = vec(0,-1,0), size = vec(10,1,10)*scalefactor, color = color.white)
 
 g = vec(0, -9.8, 0)
 Fg = raindrop.mass*g
@@ -19,7 +18,7 @@ dt = 0.01
 motion_graph = graph(title = "Velocity-Time", xtitle = "t", ytitle = "v")
 raindrop_v = gcurve()
 
-while mag(raindrop.pos - ground.pos) > raindrop.radius - ground.pos.y:
+while mag(raindrop.pos) > raindrop.radius:
     rate(1/dt)
     raindrop_v.plot(pos = (t, -raindrop.v.y))
     airdrag = -k*raindrop.v
