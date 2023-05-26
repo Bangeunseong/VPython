da7acc2630f1308d42ef4aa0ac224354f0bc9782
Bangeunseong <bangeunseong@naver.com>
2023-05-26 23:05:55 +0900

    Commit

--- Assignment/Assignment5.py
+++ Assignment/Assignment5.py
@@ -1,14 +1,55 @@
 from vpython import *
 
-tanklowerbody = box(pos=vec(0,0,0), size=vec(1.6,0.4,1), color=vec(0.2,0.6,0.2))
-tankupperbody = box(pos=tanklowerbody.pos + vec(0,0.32,0), size=vec(1,0.24,0.6), color=vec(0.2,0.6,0.2))
-tankcannon = box(pos=tankupperbody.pos + vec(0.4,0,0), size=vec(1.2,0.1,0.1), color=vec(0.2,0.6,0.2))
+def keydownInput(evt):
+    s = evt.key
+    if 'down' in s:
+        spring.ks = 12000
 
-scene.center = tankupperbody.pos
-scene.camera.pos = tankupperbody.pos + vec(0,0.5,0)
-scene.camera.axis = vec(0.7,-0.3,0)
+def keyupInput(evt):
+    s = evt.key
+    if 'down' in s:
+        spring.ks = 6000
 
-mouse_pos = scene.mouse.pos
-scene.camera.rotate(angle=radians(45), axis=vec(0,1,0), origin=tankupperbody.pos)
-tankupperbody.rotate(angle=radians(45), axis=vec(0,1,0))
-tankcannon.rotate(angle=radians(45), axis=vec(0,1,0), origin=tanklowerbody.pos)
+building_floor = box(pos = vec(-2.5,-0.05,0), size = vec(8,0.1,8), color = color.white)
+scoreboard = box(pos = vec(-4,7.5,0), size = vec(0.5,15,0.5), color = color.cyan)
+showwinner = label(pos = scene.center, text="Win!", box = False, visible = False, color = color.red)
+
+spring = helix(pos = vec(0,5,0), axis = vec(0,-1,0), v = vec(0,0,0), m = 30, ks = 6000, radius = 0.1, rate = 0.05, degree = 0, thickness = 0.03, coils = 20, color = color.cyan)
+pedalstep = box(pos = spring.pos, size = vec(1.4,0.05,0.5), color = color.orange)
+center_piller = cylinder(pos = spring.pos, axis = vec(0,3,0), radius = 0.1, color = color.orange)
+handle = cylinder(pos = spring.pos + center_piller.axis - vec(0.7,0,0), axis = vec(1.4,0,0), radius = 0.1, color = color.orange)
+
+t = 0
+dt = 0.001
+r0 = 1
+kd = 70
+g = vec(0,-9.8,0)
+
+scene.center = spring.pos
+scene.bind("keydown", keydownInput)
+scene.bind("keyup", keyupInput)
+
+while True:
+    rate(1/dt)
+    if handle.pos.y > 15:
+        showwinner.visible = True
+        break
+    Fg = spring.m*g
+    Fnet = Fg
+    if (spring.pos + spring.axis).y <= 0:
+        rhat = norm(spring.axis)
+        s = r0 - mag(spring.axis)
+        Fspr = -spring.ks*s*rhat
+        Fdamp = -kd*dot(spring.v, rhat)*rhat
+        Fnet = Fnet + Fspr + Fdamp
+
+    spring.v += Fnet/spring.m*dt
+    spring.pos += spring.v*dt
+    if spring.pos.y < 1:
+        spring.axis = -norm(spring.pos)*mag(spring.pos)
+    else:
+        spring.axis = -norm(spring.pos)
+    pedalstep.pos = spring.pos
+    center_piller.pos = spring.pos
+    handle.pos = spring.pos + center_piller.axis - vec(0.7,0,0)
+    t += dt
